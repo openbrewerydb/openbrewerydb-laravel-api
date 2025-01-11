87a5c38de3d01a126442676ca4b151d96d5d9556
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-01-10 21:39:18 -0500

    Add /about endpoint to provide brewery statistics

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/about', function () {
+    return response()->json([
+        'name' => 'Open Brewery DB API',
+        'version' => '1.0.0',
+        'data' => [
+            'records' => DB::table('breweries')->count(),
+            'last_updated' => DB::table('breweries')->max('updated_at'),
+        ],
+    ]);
+});
+
 require __DIR__ . '/api/v1/routes.php';
