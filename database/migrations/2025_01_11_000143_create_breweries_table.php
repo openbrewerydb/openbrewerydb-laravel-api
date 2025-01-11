ffddfa350c28485c4959a66f854f0c76e6950371
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-01-11 10:41:57 -0500

    updated brewery ddl

--- database/migrations/2025_01_11_000143_create_breweries_table.php
+++ database/migrations/2025_01_11_000143_create_breweries_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
             $table->uuid('id')->primary();
 
             $table->string('name');
-            $table->string('type')->default('unknown');
+            $table->string('type')->default('unknown')->index();
             $table->string('address_1')->nullable();
             $table->string('address_2')->nullable();
             $table->string('address_3')->nullable();
-            $table->string('city')->nullable();
-            $table->string('province')->nullable();
-            $table->string('country')->nullable();
+            $table->string('city');
+            $table->string('province');
+            $table->string('country');
             $table->string('postal_code')->nullable();
             $table->string('website_url')->nullable();
             $table->string('phone_number')->nullable();
