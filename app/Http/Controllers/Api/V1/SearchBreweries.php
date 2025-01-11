0e2bd09d93b11906a27775249582129fe36bdb42
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-01-10 23:04:07 -0500

    Add search endpoint for breweries with validation

---
+++ app/Http/Controllers/Api/V1/SearchBreweries.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Brewery;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class SearchBreweries extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'query' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $breweries = Brewery::search(query: urldecode($request->input('query')))
+            ->simplePaginate($request->input('per_page', 50));
+
+        return response()->json($breweries);
+    }
+}
--- routes/api/v1/routes.php
+++ routes/api/v1/routes.php
@@ -4,11 +4,13 @@ use App\Http\Controllers\Api\V1\Autocomplete;
 use App\Http\Controllers\Api\V1\GetBrewery;
 use App\Http\Controllers\Api\V1\ListBreweries;
 use App\Http\Controllers\Api\V1\RandomBrewery;
+use App\Http\Controllers\Api\V1\SearchBreweries;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::get('/breweries', ListBreweries::class);
     Route::get('/breweries/autocomplete', Autocomplete::class);
     Route::get('/breweries/random', RandomBrewery::class);
+    Route::get('/breweries/search', SearchBreweries::class);
     Route::get('/breweries/{id}', GetBrewery::class);
 });
