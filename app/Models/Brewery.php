a6275cb7e416893f629864e36d3d81ac94a32f18
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-01-15 09:14:07 -0500

    Add distance filtering for brewery listings

--- app/Models/Brewery.php
+++ app/Models/Brewery.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Enums\BreweryType;
+use App\Models\Traits\V1\BreweryFilters as v1BreweryFilters;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -12,7 +13,7 @@ use Laravel\Scout\Searchable;
 class Brewery extends Model
 {
     /** @use HasFactory<\Database\Factories\BreweryFactory> */
-    use HasFactory, HasUuids, Searchable, SoftDeletes;
+    use HasFactory, HasUuids, Searchable, SoftDeletes, v1BreweryFilters;
 
     /**
      * Get the attributes that should be cast.
---
+++ app/Models/Traits/V1/BreweryFilters.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\Traits\V1;
+
+use Illuminate\Support\Facades\DB;
+
+trait BreweryFilters
+{
+    /**
+     * Order results by distance from given coordinates. Use "6371" for kilometers or "3959" for miles.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param float $latitude
+     * @param float $longitude
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeOrderByDistance($query, $latitude, $longitude)
+    {
+        $haversine = "(3959 * acos(cos(radians($latitude))
+                        * cos(radians(latitude))
+                        * cos(radians(longitude)
+                        - radians($longitude))
+                        + sin(radians($latitude))
+                        * sin(radians(latitude))))";
+
+        return $query->selectRaw("{$haversine} AS distance")
+            ->whereNotNull('latitude')
+            ->whereNotNull('longitude')
+            ->orderBy('distance');
+    }
+}
