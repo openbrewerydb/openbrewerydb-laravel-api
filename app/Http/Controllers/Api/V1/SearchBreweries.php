c86312a9c3517a51f973b0d61bb70c400eaba6ba
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-01-28 21:07:10 -0500

    Remove unused BreweryCollection import from SearchBreweries controller

--- app/Http/Controllers/Api/V1/SearchBreweries.php
+++ app/Http/Controllers/Api/V1/SearchBreweries.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\V1\BreweryCollection;
 use App\Http\Resources\V1\BreweryResource;
 use App\Models\Brewery;
 use Illuminate\Http\Request;
