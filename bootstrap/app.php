44146cfd11c65b3c75f3d1c963e9294149c839e7
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-02-10 17:55:39 -0500

    [Chore] Remove unnecessary middleware for API requests

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
-
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
