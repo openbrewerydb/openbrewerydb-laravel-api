b41d70802e3d8b085310be01fb20688d09d9414f
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-03-08 07:51:15 -0500

    [Chore] Replace HasUuids with HasVersion4Uuids in Brewery model

--- app/Models/Brewery.php
+++ app/Models/Brewery.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use App\Enums\BreweryType;
 use App\Models\Traits\V1\BreweryFilters as v1BreweryFilters;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Concerns\HasVersion4Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Scout\Searchable;
@@ -12,7 +12,7 @@ use Laravel\Scout\Searchable;
 class Brewery extends Model
 {
     /** @use HasFactory<\Database\Factories\BreweryFactory> */
-    use HasFactory, HasUuids, Searchable, v1BreweryFilters;
+    use HasFactory, HasVersion4Uuids, Searchable, v1BreweryFilters;
 
     /**
      * Indicates if the model should be timestamped.
