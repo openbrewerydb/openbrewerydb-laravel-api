5e5d3ac8cc6df28b14b3802f005ae5a7e5419362
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-03-20 13:32:16 -0400

    started work on generating API docs using Scamble

--- app/Http/Controllers/Api/V1/GetBrewery.php
+++ app/Http/Controllers/Api/V1/GetBrewery.php
@@ -5,16 +5,22 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\BreweryResource;
 use App\Models\Brewery;
+use Dedoc\Scramble\Attributes\Group;
+use Dedoc\Scramble\Attributes\PathParameter;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Cache;
 
+#[Group(name: 'Breweries', weight: 1)]
 class GetBrewery extends Controller
 {
     /**
-     * Handle the incoming request.
+     * Get brewery
+     *
+     * Get a brewery by ID.
      */
+    #[PathParameter('id', description: 'UUID of the brewery.', type: 'string', example: '9fb357eb-965e-4920-81d0-256910248fc0', required: true)]
     public function __invoke(Request $request, string $id): JsonResponse
     {
         $brewery = Cache::remember('brewery_'.$id, 300, function () use ($id) {
