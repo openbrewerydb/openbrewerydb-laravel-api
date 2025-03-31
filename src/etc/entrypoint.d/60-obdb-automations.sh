8f13b0e8e3d2cae83616467b85aaa7afa5ae820b
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-03-30 20:35:59 -0400

    [Feature] Add automated brewery data import in entrypoint script

--- src/etc/entrypoint.d/60-obdb-automations.sh
+++ src/etc/entrypoint.d/60-obdb-automations.sh
@@ -1,6 +1,14 @@
 ############################################################################
 # artisan app:refresh-search-indexes
 ############################################################################
+if [ "${AUTORUN_OBDB_IMPORT_BREWERIES:=false}" = "true" ]; then
+    echo "🔍 Importing latest brewery data..."
+    php "$APP_BASE_DIR/artisan" app:import-breweries
+fi
+
+############################################################################
+# artisan app:refresh-search-indexes
+############################################################################
 if [ "${AUTORUN_OBDB_REFRESH_SEARCH_INDEXES:=false}" = "true" ]; then
     echo "🔍 Refreshing search indexes..."
     php "$APP_BASE_DIR/artisan" app:refresh-search-indexes
