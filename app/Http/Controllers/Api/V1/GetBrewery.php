9e67b38534b12669d3b894e6617d8180bacb8802
Chris Mears <chris.mears@gmail.com>
2025-04-18 21:16:53 -0700

    [Fix] Update Cache-Control header separator from semicolons to commas

--- app/Http/Controllers/Api/V1/GetBrewery.php
+++ app/Http/Controllers/Api/V1/GetBrewery.php
@@ -24,7 +24,7 @@ class GetBrewery extends Controller
         return response()->json(
             data: $brewery,
             status: Response::HTTP_OK,
-            headers: ['Cache-Control' => 'public; max-age=300; etag'],
+            headers: ['Cache-Control' => 'public, max-age=300, etag'],
         );
     }
 }
