d91202fb6151553d81a06374f5037df61ebdbfe8
Alex Justesen <alexjustesen@users.noreply.github.com>
2026-08-04 18:36:05 -0400

    task: 2026-08-04 dependency updates (#148)
    Co-authored-by: Alex Justesen <1144087+alexjustesen@users.noreply.github.com>

--- run-tests-with-coverage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create coverage directory if it doesn't exist
-mkdir -p coverage
-
-# Run Pest with coverage
-XDEBUG_MODE=coverage ./vendor/bin/sail artisan test --coverage --coverage-html coverage/html --coverage-clover coverage/clover.xml
